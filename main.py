1601032c4a39b73f9d27afca7174df984e20acbd
claudiney63 <claudineyryan75@gmail.com>
2024-05-09 16:51:22 -0300

    add scores in output.txt

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import write_alignment_to_file as waf
 def main(seq1, seq2, match_score, miss_score, gap_penalty, output_file):
     scores_matrix = sw.smith_waterman(seq1, seq2, match_score, miss_score, gap_penalty)
     aligned_seq1, aligned_seq2 = tb.trace_back(scores_matrix, seq1, seq2, gap_penalty)
-    waf.write_alignment_to_file(aligned_seq1, aligned_seq2, scores_matrix, seq1, seq2, output_file)
+    waf.write_alignment_to_file(aligned_seq1, aligned_seq2, scores_matrix, seq1, seq2, output_file, match_score, miss_score, gap_penalty)
 
 if __name__ == "__main__":
     input_file = "input.txt"
--- write_alignment_to_file.py
+++ write_alignment_to_file.py
@@ -1,4 +1,4 @@
-def write_alignment_to_file(aligned_seq1, aligned_seq2, scores_matrix, seq1, seq2, filename):
+def write_alignment_to_file(aligned_seq1, aligned_seq2, scores_matrix, seq1, seq2, filename, match_score, miss_score, gap_penalty):
     with open(filename, 'w') as file:
         file.write("Matriz de pontuacoes:\n")
         cont = 1
@@ -12,6 +12,7 @@ def write_alignment_to_file(aligned_seq1, aligned_seq2, scores_matrix, seq1, seq
             # else:
             #     file.write("  " + " ".join(map(str, scores_matrix[i])) + "\n")
         file.write("\tU\t" + "\t".join(seq2) + "\n")
+        file.write("\nMatch = " +str(match_score)+ "; Miss = "+str(miss_score)+ "; Gap = "+str(gap_penalty)+"; \n")
         file.write("\nAlinhamento 1: " + aligned_seq1 + "\n")
         file.write("Alinhamento 2: " + aligned_seq2 + "\n")
         file.write("\nNumero chamada: 8 \n")
